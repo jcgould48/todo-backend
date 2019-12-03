aa80d1360d9c441bf74b3bf350c147fe3968f88d
Colin Jaffe <balloonasaurus@gmail.com>
2019-12-03 14:36:22 -0500

    Style fix.

--- main.test.js
+++ main.test.js
@@ -25,37 +25,31 @@ beforeEach(() => {
   originalTodos = [
     {
       text: 'Christmas shopping',
-      
       complete: false,
       priority: 1,
     },
     {
       text: `make doctor's appointment`,
-      
       complete: true,
       priority: 2,
     },
     {
       text: 'binge watch The Office',
-      
       complete: false,
       priority: 2,
     },
     {
       text: 'research dinosaurs',
-      
       complete: false,
       priority: 1,
     },
     {
       text: 'fire Colin',
-      
       complete: true,
       priority: 2,
     },
     {
       text: 'start novel',
-      
       complete: true,
       priority: 1,
     },
--- todos.js
+++ todos.js
@@ -1,37 +1,31 @@
 const todos = [
   {
     text: 'Christmas shopping',
-    
     complete: false,
     priority: 1,
   },
   {
     text: `make doctor's appointment`,
-    
     complete: true,
     priority: 2,
   },
   {
     text: 'binge watch The Office',
-    
     complete: false,
     priority: 2,
   },
   {
     text: 'research dinosaurs',
-    
     complete: false,
     priority: 1,
   },
   {
     text: 'fire Colin',
-    
     complete: true,
     priority: 2,
   },
   {
     text: 'start novel',
-    
     complete: true,
     priority: 1,
   },
